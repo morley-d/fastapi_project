de1d88099f466a1f0a7297be685f1b13da4cea05
Andrey <21elektro87@gmail.com>
2024-01-13 23:06:50 +0600

    fix

--- app/main.py
+++ app/main.py
@@ -7,22 +7,6 @@ from pydantic import BaseModel
 app = FastAPI()
 
 
-class HotelSearchArgs:
-    def __init__(
-        self,
-        location: str,
-        date_from: date,
-        date_to: date,
-        stars: Optional[int] = Query(None, ge=1, le=5),
-        has_spa: Optional[bool] = None
-    ):
-        self.location = location
-        self.date_from = date_from
-        self.date_to = date_to
-        self.stars = stars
-        self.has_spa = has_spa
-
-
 class ShemaHotel(BaseModel):
     adress: str
     name: str
