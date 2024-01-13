f55d5cdc2e6bbcb277ec1bd73da9d32b5d31c64b
Andrey <21elektro87@gmail.com>
2024-01-13 23:01:20 +0600

    create class for args

--- app/main.py
+++ app/main.py
@@ -1,10 +1,26 @@
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Query, Depends
 from typing import Optional
 from datetime import date
 from pydantic import BaseModel
 
 
 app = FastAPI()
+
+
+class HotelSearchArgs:
+    def __init__(
+        self,
+        location: str,
+        date_from: date,
+        date_to: date,
+        stars: Optional[int] = Query(None, ge=1, le=5),
+        has_spa: Optional[bool] = None
+    ):
+        self.location = location
+        self.date_from = date_from
+        self.date_to = date_to
+        self.stars = stars
+        self.has_spa = has_spa
 
 
 class ShemaHotel(BaseModel):
@@ -16,12 +32,8 @@ class ShemaHotel(BaseModel):
 
 @app.get("/hotels/")
 def get_hotels(
-    location: str,
-    date_from: date,
-    date_to: date,
-    stars: Optional[int] = Query(None, ge=1, le=5),
-    has_spa: Optional[bool] = None
-) -> list[ShemaHotel]:
+    search_args: HotelSearchArgs = Depends()
+):
     hotels = [
         {
             "adress": "ул.Гагарина, 1, Алтай",
