7d49d542c1e89fd7f3d2b63a24a5f49468b04bf0
morley-d <103437424+morley-d@users.noreply.github.com>
2024-01-14 23:30:20 +0600

    Update database.py

--- app/database.py
+++ app/database.py
@@ -8,3 +8,10 @@ DB_PASS = "postgres"
 DB_NAME = "postgres"
 
 DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+engine = create_async_engine(DATABASE_URL)
+
+async_session_maker = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+
+class Base(DeclarativeBase):
+    pass
