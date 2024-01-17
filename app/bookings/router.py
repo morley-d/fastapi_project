cd3c83357572f978c3cea74204175202ae46090d
San <andreal87@mail.ru>
2024-01-17 16:43:43 +0600

    add shema

--- app/bookings/router.py
+++ app/bookings/router.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 from sqlalchemy import select
 from app.bookings.dao import BookingDAO
+from app.bookings.shemas import ShemaBooking
 from app.database import async_session_maker
 from app.bookings.models import Bookings
 
@@ -12,11 +13,11 @@ router = APIRouter(
 
 
 @router.get("")
-async def get_bookings():
+async def get_bookings() -> list[ShemaBooking]:
     async with async_session_maker() as session:
         return await BookingDAO.find_all()
 
 
-@router.get("/{booking_id}")
-def get_booking(booking_id):
-    pass
+# @router.get("/{booking_id}")
+# def get_booking(booking_id):
+#     pass
