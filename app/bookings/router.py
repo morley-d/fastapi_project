5e5d674294865e1b5f3220e7954bbf20df94aabd
San <andreal87@mail.ru>
2024-01-17 15:26:48 +0600

    create router and dao

---
+++ app/bookings/router.py
@@ -0,0 +1,22 @@
+from fastapi import APIRouter
+from sqlalchemy import select
+from app.bookings.dao import BookingDAO
+from app.database import async_session_maker
+from app.bookings.models import Bookings
+
+
+router = APIRouter(
+    prefix="/bookings",
+    tags=["Бронирования"]
+)
+
+
+@router.get("")
+async def get_bookings():
+    async with async_session_maker() as session:
+        return BookingDAO.find_all()
+
+
+@router.get("/{booking_id}")
+def get_booking(booking_id):
+    pass
