d9f64a13cd4c42ce025e3440967b2014d84f93aa
San <andreal87@mail.ru>
2024-01-18 16:37:07 +0600

    add insert user for dao

--- app/users/router.py
+++ app/users/router.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
+from app.users.auth import get_password_hash
 from app.users.dao import UsersDAO
 from app.users.shemas import ShemaUserRegister
 
@@ -11,4 +12,8 @@ router = APIRouter(
 
 @router.post("/register")
 async def register_user(user_data: ShemaUserRegister):
-    existing_user = await UsersDAO.find_one_or_none
+    existing_user = await UsersDAO.find_one_or_none(email=user_data.email)
+    if existing_user:
+        raise HTTPException(status_code=500)
+    hashed_password = get_password_hash(user_data.password)
+    await UsersDAO.add(email=user_data.email, hashed_password=hashed_password)
